3dbc0c14eeb6c370bb34bd3967a945bd76d71829
superman566 <guochaohetony@gmail.com>
2022-10-18 02:03:54 -0400

    add sass

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
-const App = () => (
-  <div className="categories-container">
-    <div className="category-container">
-      <div className="category-body-container">
-        <h2>Hats</h2>
-        <p>Shop Now</p>
-      </div>
+import './categories.styles.scss';
+
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      subtitle: '',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      subtitle: '',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      subtitle: '',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      subtitle: '',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      subtitle: '',
+    }
+  ];
+
+  return (
+    <div className="categories-container">  
+        {
+          categories.map(category => (
+          <div className="category-container" key={category.id}>
+            <div className="background-image" />
+            <div className="category-body-container">
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+          ))
+        }
     </div>
-    <div className="category-container">
-      <div className="category-body-container">
-        <h2>Jackets</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-    <div className="category-container">
-      <div className="category-body-container">
-        <h2>Sneakers</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-    <div className="category-container">
-      <div className="category-body-container">
-        <h2>Womens</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-    <div className="category-container">
-      <div className="category-body-container">
-        <h2>Mens</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-  </div>
-);
+  )
+};
 
 export default App;
