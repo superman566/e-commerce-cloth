76e3c46116fc65daec2e728b6d3eadfd011e00b6
superman566 <guochaohetony@gmail.com>
2022-10-21 23:17:03 -0400

    init routes

---
+++ src/routes/header/header.component.jsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import "./header.styles.scss";
+
+const Header = ()=>{
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+          <Logo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to='/shop' > Shop</Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+}
+
+export default Header;
